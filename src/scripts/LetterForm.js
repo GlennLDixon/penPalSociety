43588c0a5e8e546130a6b83e350bae87c5e53b66
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-01-04 22:26:55 -0600

    got the options for arthurs to display but running into an issue with the recipients

--- src/scripts/LetterForm.js
+++ src/scripts/LetterForm.js
@@ -2,12 +2,29 @@ import { getArthurs, getRecipients } from "./dataAccess.js"
 
 const mainContainer = document.querySelector("#container")
 
-mainContainer.addEventListener('click', () => {
+mainContainer.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "sendLetter") {
+        // Get what the user typed into the form fields
+        const userDescription = document.querySelector("input[name='serviceDescription']").value
+        const userAddress = document.querySelector("input[name='letter']").value
+        const userBudget = document.querySelector("input[name='serviceBudget']").value
+        const userDate = document.querySelector("input[name='serviceDate']").value
 
+        // Make an object out of the user input
+        const dataToSendToAPI = {
+            description: userDescription,
+            address: userAddress,
+            budget: userBudget,
+            neededBy: userDate
+        }
+
+        // Send the data to the API for permanent storage
+        sendRequest(dataToSendToAPI)
+    }
 })
 
-mainContainer.addEventListener('chainge', ()=> {
-    
+mainContainer.addEventListener('change', ()=> {
+
 
 })
 
@@ -16,16 +33,42 @@ export const LetterForm = () => {
     let recipients = getRecipients()
     let html = `
         <div class="field">
-        <label class="label" for="Arthur">Arthur</label>
-        <select name="arthurs" id="arthurs"><option value="${arthurs.id}"><${arthurs.name}</option></select>
+        <label class="label" for="letterArthur">Arthur</label>
+        <select name="arthurs" id="arthurs">
+        <option value="">choose</option>
+        ${
+            arthurs.map(
+                arthur => {
+                    return `<option value="${arthur.id}">${arthur.name}</option>`
+                }
+            ).join("")}
+        </select>
         </div>
         <div class="field">
-            <label class="label" for="serviceAddress">Address</label>
-            <input type="text" name="serviceAddress" class="input" />
+            <label class="label" for="letterInput">Letter</label>
+            <input type="text" name="letter" class="input" />
+        </div>
+        <div>
+            <label class="label" for="letterTopics">Topics</label>
+            <ul>
+            <li><input type="radio" name="Business"/>Business</li>
+            <li><input type="radio" name="Friendly"/>Friendly</li>
+            <li><input type="radio" name="Family"/>Family</li>
+            <li><input type="radio" name="Congratulations"/>Congratulations</li>
+            <li><input type="radio" name="Condolences"/>Condolences</li>
+            </ul>
         </div>
         <div class="field">
-        <label class="label" for="Arthur">Arthur</label>
-        <select name="arthurs" id="arthurs"><option value="${recipients.id}"><${recipients.name}</option></select>
+        <label class="label" for="Arthur">Recipient</label>
+        <select name="recipient" id="recipient">
+        <option value="">choose</option>
+        ${
+            recipients.map(
+                recipient => {
+                    return `<option value="${recipient.id}"><${recipient.name}</option>`
+                }
+            ).join("")}
+            </select>
         </div>
         
 
